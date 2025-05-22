263ecaf955d2cf5c70da0944e8f823cdab826b0b
MidnightJump <desmond597@outlook.com>
2025-05-22 11:29:51 +0800

    add:add failed tests

--- test/internal/auth/auth_test.go
+++ test/internal/auth/auth_test.go
@@ -83,15 +83,15 @@ func TestGetAPIKey(t *testing.T) {
 			want:    "",
 			wantErr: true,
 		},
-		//// 包含多个空格的key
-		//{
-		//	name: "key with spaces",
-		//	headers: http.Header{
-		//		"Authorization": {"ApiKey key with spaces"},
-		//	},
-		//	want:    "key with spaces",
-		//	wantErr: false,
-		//},
+		// 包含多个空格的key
+		{
+			name: "key with spaces",
+			headers: http.Header{
+				"Authorization": {"ApiKey key with spaces"},
+			},
+			want:    "key with spaces",
+			wantErr: false,
+		},
 		// 区分大小写
 		{
 			name: "case sensitive",
